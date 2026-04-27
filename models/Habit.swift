69c02d2fecbf8c7895a0e987020f4c3cb21fc4e1
Ahmad <ahmadalhayel20@gmail.com>
2026-04-27 16:11:23 +0200

    Model: Define Habit model with reminder properties

--- models/Habit.swift
+++ models/Habit.swift
@@ -1,8 +1,34 @@
-//
-//  Habit.swift
-//  
-//
-//  Created by Ahmad Alhayel on 4/27/26.
-//
-
+import SwiftData
 import Foundation
+
+@Model
+final class Habit {
+    var name: String
+    var icon: String
+    var color: String
+    var createdAt: Date
+
+    var reminderEnabled: Bool
+    var reminderHour: Int
+    var reminderMinute: Int
+
+    @Relationship(deleteRule: .cascade)
+    var logs: [HabitLog] = []
+
+    init(name: String,
+         icon: String,
+         color: String,
+         createdAt: Date = Date(),
+         reminderEnabled: Bool = false,
+         reminderHour: Int = 8,
+         reminderMinute: Int = 0) {
+
+        self.name = name
+        self.icon = icon
+        self.color = color
+        self.createdAt = createdAt
+        self.reminderEnabled = reminderEnabled
+        self.reminderHour = reminderHour
+        self.reminderMinute = reminderMinute
+    }
+}
