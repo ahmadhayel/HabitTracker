c92b04e5422f97b51a2758a25a3b9605ef4feb7a
Ahmad <ahmadalhayel20@gmail.com>
2026-04-29 20:51:22 +0200

    Feat: Implement habit creation logic and basic UI in AddHabitSheet

--- views/AddHabitSheet.swift
+++ views/AddHabitSheet.swift
@@ -1,18 +1,40 @@
-//
-//  AddHabitSheet.swift
-//  
-//
-//  Created by Ahmad Alhayel on 4/27/26.
-//
-
 import SwiftUI
+import SwiftData
 
 struct AddHabitSheet: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
+    
+    @State private var name = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Habit name", text: $name)
+            }
+            .navigationTitle("New Habit")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {
+                        let newHabit = Habit(
+                            name: name,
+                            icon: "star",
+                            color: "blue"
+                        )
+                        
+                        context.insert(newHabit)
+                        dismiss()
+                    }
+                    .disabled(name.isEmpty)
+                }
+            }
+        }
     }
-}
-
-#Preview {
-    AddHabitSheet()
 }
